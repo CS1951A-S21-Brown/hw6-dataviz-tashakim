7c4116c7cd5c9a7f640c3d5c144f8a5cdb1dd48a
Tasha Kim <tasha_kim@brown.edu>
2021-03-24 23:59:22 -0400

    Display some text

--- main.js
+++ main.js
@@ -8,3 +8,37 @@ let graph_1_width = (MAX_WIDTH / 2) - 10, graph_1_height = 250;
 let graph_2_width = (MAX_WIDTH / 2) - 10, graph_2_height = 275;
 let graph_3_width = MAX_WIDTH / 2, graph_3_height = 575;
 
+let svg1 = d3.select('#graph1')
+.append("svg")
+.attr("width", graph_1_width)
+.attr("height", graph_1_height)
+.append("g")
+.attr("transform", `translate(${margin}, ${margin})`)
+
+svg1.append("text")
+        .attr("transform", `translate(100,100)`)       // HINT: Place this at the bottom middle edge of the graph - use translate(x, y) that we discussed earlier
+        .style("text-anchor", "middle")
+        .text("Count");
+// Adds y-axis label
+svg1.append("text")
+    .attr("transform", `translate(140,140)`)       // HINT: Place this at the center left edge of the graph - use translate(x, y) that we discussed earlier
+    .style("text-anchor", "middle")
+    .text("Artist");
+// Adds chart title
+svg1.append("text")
+    .attr("transform", `translate(200,200)`)       // HINT: Place this at the top middle edge of the graph - use translate(x, y) that we discussed earlier
+    .style("text-anchor", "middle")
+    .style("font-size", 15)
+    .text("Top Artists in Billboard 100 Charts");
+
+let countRef = svg.append("g");
+
+d3.csv("../data/video_games.csv").then(function(data){
+    cleanedData = cleanData(
+        data, 
+        function(x,y) {
+            return parseInt(y["count"], 10) - parseInt(x["count"], 10)}, 
+        NUM_EXAMPLES)
+    console.log(data)
+        
+})
